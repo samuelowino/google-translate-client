b4a6da95c763a002b4e84f10c469b10f5577c68e
mwangidennis1 <mwangedennis05@gmail.com>
2025-01-16 06:47:44 +0300

    add an Eventbus

---
+++ desktop/src/main/java/org/mwangi/desktop/util/EventBus.java
@@ -0,0 +1,65 @@
+package org.mwangi.desktop.util;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleIntegerProperty;
+import org.mwangi.desktop.dialogs.NotificationDialog;
+import org.mwangi.desktop.payload.TranslationResponse;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+
+public class EventBus {
+
+
+    private final SimpleIntegerProperty activityCount=new SimpleIntegerProperty();
+    private final PublishSubject<TranslationResponse> responses=PublishSubject.create();
+    private final Executor uiexecutor;
+    public EventBus(Executor uiexecutor){
+        this.uiexecutor=uiexecutor;
+    }
+    private void startActivity(){
+        if(Platform.isFxApplicationThread()){
+            activityCount.set(activityCount.get() +1);
+        }else{
+            CompletableFuture.runAsync(()->activityCount.set(activityCount.get() +1),uiexecutor);
+        }
+    }
+    private void stopActivity(){
+        if(Platform.isFxApplicationThread()){
+            activityCount.set(activityCount.get() -1);
+        }else{
+            CompletableFuture.runAsync(()->activityCount.set(activityCount.get()-1),uiexecutor);
+        }
+    }
+
+
+    public SimpleIntegerProperty getActivityCount() {
+        return activityCount;
+    }
+
+    public   <T> CompletableFuture<T> activity(CompletableFuture<T> completableFuture){
+            startActivity();
+             return completableFuture.whenComplete((_,throwable)->{
+                 stopActivity();
+                 if(throwable != null){
+                     throwable.printStackTrace();
+                     CompletableFuture.runAsync(()->new NotificationDialog(throwable.toString()).show(),uiexecutor);
+                 }
+        });
+    }
+    public SimpleIntegerProperty activityCountProperty() {
+        return activityCount;
+    }
+    public  void pub(TranslationResponse res){
+
+        responses.onNext(res);
+    }
+
+    public Observable<TranslationResponse> getD(){
+       return responses;
+    }
+
+}
